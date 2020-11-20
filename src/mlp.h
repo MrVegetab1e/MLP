fe45524363fdfcb5ec14202f3368e0a750f91959
Vegetab1e <lizhi88038@gmail.com>
2020-11-20 16:37:21 +0800

    Start coding

---
+++ src/mlp.h
@@ -0,0 +1,13 @@
+/** @file mlp.h
+ *  @brief Header of MLP skeleton.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+
